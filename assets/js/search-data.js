6fbbff71f9a44b43251b5e70bf21b5a6d834fdd1
Chazae <Chazae@users.noreply.github.com>
2025-01-06 06:20:03 +0000

    Deploying to gh-pages from @ Chazae/Chazae.github.io@2289ea01c5b3e64b9e7ec87ade9956c77e759f6f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -24,7 +24,7 @@ ninja.data = [{
             window.location.href = "/publications/";
           },
         },{id: "nav-projects",
-          title: "projects",
+          title: "Projects",
           description: "A collection of my personal projects",
           section: "Navigation",
           handler: () => {
