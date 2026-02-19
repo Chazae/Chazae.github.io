93037ea55ac67ef0e479eacea0f736ffcc5b8e45
Chazae <Chazae@users.noreply.github.com>
2026-02-19 05:32:36 +0000

    Deploying to gh-pages from @ Chazae/Chazae.github.io@a9ea61432e6729f26c49c0d96e2fbb6718cd7b80 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -41,17 +41,6 @@ ninja.data = [{
           window.location.href = "/blog/2024/photo-gallery/";
         
       },
-    },{id: "post-a-post-with-formatting-and-links",
-      
-        title: "a post with formatting and links",
-      
-      description: "march &amp; april, looking forward to summer",
-      section: "Posts",
-      handler: () => {
-        
-          window.location.href = "/blog/2015/formatting-and-links/";
-        
-      },
     },{id: "news-i-like-turtle-wow",
           title: 'I like Turtle Wow',
           description: "",
